30c336dac731ed6da75a8bc0a412c0e3fe1b485f
2504488720 <2504488720@qq.com>
2020-12-26 21:18:46 +0800

    Gameplay Ability System Init

---
+++ Source/ACTGame/Public/Abilities/BaseAbility_Montage.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Abilities/GameplayAbility_Montage.h"
+#include "BaseAbility_Montage.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTGAME_API UBaseAbility_Montage : public UGameplayAbility_Montage
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/ACTGame/Public/BaseCharacter.h
@@ -0,0 +1,38 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "AbilitySystemComponent.h"
+#include "BaseCharacter.generated.h"
+
+UCLASS()
+class ACTGAME_API ABaseCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ABaseCharacter();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+	virtual void PossessedBy(AController* NewController) override;
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+public:
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Abilities, meta = (AllowPrivateAccess = "true"))
+	class UAbilitySystemComponent* AbilitySystemComponent;
+
+	UFUNCTION(BlueprintCallable)
+	class UAbilitySystemComponent* GetAbilitySystemComponent() const override
+	{
+		return AbilitySystemComponent;
+	}
+};
